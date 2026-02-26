55f218176b2ba48431079f3d19783ec0307cf53f
Frompaje <yanedwards11@gmail.com>
2026-02-26 15:07:59 -0300

    feat: enhance GetAllTicketRepository to include additional ticket fields in the selection and order results by creation date

--- src/module/tickets/repositories/getAllTicketRepository.ts
+++ src/module/tickets/repositories/getAllTicketRepository.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { desc } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { tickets } from 'src/db/ticket';
 
@@ -9,6 +10,20 @@ export class GetAllTicketRepository {
   ) {}
 
   async execute() {
-    return await this.db.select().from(tickets);
+    return await this.db
+      .select({
+        title: tickets.title,
+        category: tickets.category,
+        price: tickets.price,
+        description: tickets.description,
+        location: tickets.location,
+        date: tickets.date,
+        time: tickets.time,
+        image: tickets.image,
+        quantity: tickets.quantity,
+        max_quantity: tickets.max_quantity,
+      })
+      .from(tickets)
+      .orderBy(desc(tickets.created_at));
   }
 }
